4c7ba4d0a6ed3f76f38f283933cb9a8d854e0e26
Rogerio <rogerio.santana@df.docente.senai.br>
2023-07-07 17:19:47 -0300

    Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import Titulo from "./Titulo"
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/home";
+import Sobre from "./pages/sobre";
+import Contato from "./pages/contato";
 
 function App(){
-  return (
-    <div>
-      <Titulo cor="red" />
-      <Titulo cor="blue"/>
-      <Titulo cor="orangered"/>
-    </div>
-  ) 
+    return(
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/sobre" element={<Sobre/>}></Route>
+            <Route path="/contato" element={<Contato/>}></Route>
+          </Routes>
+        </BrowserRouter>
+        
+      </div>
+    )
 }
 
 export default App
---
+++ src/pages/Menu.jsx
@@ -0,0 +1,11 @@
+function Menu(){
+    return (
+        <div>
+            <a href="/"> Home </a>&nbsp; &nbsp; 
+            <a href="/sobre"> Sobre  </a>&nbsp;&nbsp;  
+            <a href="/contato"> Contato </a>&nbsp; &nbsp; 
+        </div>
+    )
+}
+
+export default Menu
