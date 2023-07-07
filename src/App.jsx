8f52601befba35c4383d7b1f10373678fdd7dd9a
Rogerio <rogerio.santana@df.docente.senai.br>
2023-07-07 15:47:01 -0300

    Estado (state)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Titulo from "./Titulo"
 function App(){
   return (
     <div>
-      <Titulo cor="red" nome="Vitec" paragrafo={false}/>
+      <Titulo cor="red" />
       <Titulo cor="blue"/>
       <Titulo cor="orangered"/>
     </div>
--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -1,16 +1,22 @@
-function Titulo({nome,paragrafo,cor}){
+import { useState } from "react"
+
+function Titulo({cor}){
+    const [texto,setTexto] = useState("Um titulo de estado inicial")
+    const [inputText,setInputText] = useState("")
     const soma = 10 + 10
     const urlImg = "https://nerdhits.com.br/wp-content/uploads/2023/07/naruto-uzumaki_qabz.jpg"
+
+    function clicou(){
+        setTexto(inputText)
+    }
+
+ 
+
     return (
         <div>
-            <h1 style={{color: cor}} >Oi, eu sou {nome ? nome : "Fulano"}</h1>
-            {
-                paragrafo ? 
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt impedit quasi nam delectus doloremque vero, illum, quod maxime temporibus adipisci omnis. Recusandae fugit maiores pariatur sed, quo repudiandae molestiae!</p>
-                : 
-                <p>naada</p>
-            }
-            
+            <h1 style={{color: cor}} >{texto}</h1>
+            <input type="text" onChange={(e)=>{setInputText(e.target.value)}} value={inputText} name="" id="" />
+            <button onClick={clicou} > Mudar </button>
         </div>
     )
     
